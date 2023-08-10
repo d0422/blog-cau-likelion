27594db2bfe3de6e4d783b260749046dd6dd0e5e
d0422 <rlfehd2013@naver.com>
2023-08-10 14:51:03 +0900

    feat: 썸네일 없는 경우 기본 이미지 추가

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import React from 'react';
 import { styled } from 'styled-components';
+import cauIcon from '@/image/cau사자.png';
+
 export interface feed {
   writer: string;
   title: string | undefined;
@@ -23,13 +25,20 @@ const Feed = ({ data }: { data: feed }) => {
         </WriterAndDate>
       </Content>
       <ImageContainer>
-        {data.thumbnail && (
+        {data.thumbnail ? (
           <Image
             src={data.thumbnail}
             alt="이미지"
             fill={true}
             style={{ objectFit: 'contain' }}
           />
+        ) : (
+          <Image
+            src={cauIcon}
+            alt="이미지"
+            fill={true}
+            style={{ objectFit: 'contain' }}
+          ></Image>
         )}
       </ImageContainer>
     </Wrapper>
@@ -48,6 +57,7 @@ const Wrapper = styled.div`
   margin-bottom: 30px;
   display: flex;
   gap: 20px;
+  width: 100%;
   height: 100%;
   box-shadow: rgba(0, 0, 0, 0.04) 0px 4px 16px 0px;
   transition: box-shadow 0.25s ease-in 0s, transform 0.25s ease-in 0s;
@@ -73,6 +83,19 @@ const ImageContainer = styled.div`
   min-width: 200px;
   min-height: 200px;
   object-fit: contain;
+  @media (max-width: 1440px) {
+    min-width: 200px;
+    min-height: 200px;
+  }
+
+  @media (max-width: 900px) {
+    min-width: 150px;
+    min-height: 150px;
+  }
+  @media (max-width: 360px) {
+    min-width: 100px;
+    min-height: 100px;
+  }
 `;
 const DateContainer = styled.div`
   display: flex;
