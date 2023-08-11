fdb78f59a05b5cee50072ad867a8a263d191bf25
d0422 <rlfehd2013@naver.com>
2023-08-11 18:22:58 +0900

    refactor: Rss api분리

---
+++ src/api/getRss.ts
@@ -0,0 +1,51 @@
+import { feed } from '@/components/Feed';
+import blogList from '../assets/data.json';
+import Parser from 'rss-parser';
+import { JSDOM } from 'jsdom';
+
+export const getRss = async () => {
+  const parser = new Parser();
+  return await Promise.all(
+    blogList.map(async ({ name, blog }) => {
+      const feed = await parser.parseURL(blog);
+      const result = feed.items.map((item) => {
+        return {
+          title: item.title,
+          writer: name,
+          link: item.link,
+          content: removeAdd(item?.content)
+            .replace(/<[^>]*>?/g, '')
+            .replace(/\n/g, '')
+            .trim()
+            .replace(/\s+/g, ' ')
+            .slice(0, 300),
+          thumbnail: getThumbnail(item.content),
+          date: item.isoDate,
+        };
+      });
+      return result as unknown as feed[];
+    })
+  );
+};
+
+const getThumbnail = (content: string | undefined) => {
+  const regex = /<img\s+src=(?:(['"])(.*?)\1|([^'"\s]+))/g;
+  if (content) {
+    const result = content.match(regex);
+    if (result) return result[0].split(/src=/)[1].split(/\'|\"/)[1];
+    return null;
+  }
+  return null;
+};
+
+const removeAdd = (content: string | undefined) => {
+  if (!content) return '';
+  const dom = new JSDOM(content);
+  const document = dom.window.document;
+  const revenueUnitWraps = document.querySelectorAll('.revenue_unit_wrap');
+  revenueUnitWraps.forEach((revenueUnitWrap) => {
+    revenueUnitWrap.remove();
+  });
+  const outputHTML = document.documentElement.outerHTML;
+  return outputHTML;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,11 @@
 import styled from 'styled-components';
-import Parser from 'rss-parser';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Feed, { feed } from '@/components/Feed';
-import { JSDOM } from 'jsdom';
-import blogList from '../assets/data.json';
+import { getRss } from '@/api/getRss';
 export const getStaticProps: GetStaticProps<{
   data: feed[];
 }> = async () => {
-  const parser = new Parser();
-  const parsingData = await Promise.all(
-    blogList.map(async ({ name, blog }) => {
-      const feed = await parser.parseURL(blog);
-      const result = feed.items.map((item) => {
-        return {
-          title: item.title,
-          writer: name,
-          link: item.link,
-          content: removeAdd(item?.content)
-            .replace(/<[^>]*>?/g, '')
-            .replace(/\n/g, '')
-            .trim()
-            .replace(/\s+/g, ' ')
-            .slice(0, 300),
-          thumbnail: getThumbnail(item.content),
-          date: item.isoDate,
-        };
-      });
-      return result as unknown as feed[];
-    })
-  );
-
+  const parsingData = await getRss();
   return {
     props: {
       data: parsingData
@@ -42,28 +18,6 @@ export const getStaticProps: GetStaticProps<{
     },
     revalidate: 300,
   };
-};
-
-const getThumbnail = (content: string | undefined) => {
-  const regex = /<img\s+src=(?:(['"])(.*?)\1|([^'"\s]+))/g;
-  if (content) {
-    const result = content.match(regex);
-    if (result) return result[0].split(/src=/)[1].split(/\'|\"/)[1];
-    return null;
-  }
-  return null;
-};
-
-const removeAdd = (content: string | undefined) => {
-  if (!content) return '';
-  const dom = new JSDOM(content);
-  const document = dom.window.document;
-  const revenueUnitWraps = document.querySelectorAll('.revenue_unit_wrap');
-  revenueUnitWraps.forEach((revenueUnitWrap) => {
-    revenueUnitWrap.remove();
-  });
-  const outputHTML = document.documentElement.outerHTML;
-  return outputHTML;
 };
 
 export default function Home({
