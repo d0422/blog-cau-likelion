158f8a3e36dacd30f6b69d9551d3dab027a93725
d0422 <rlfehd2013@naver.com>
2023-08-24 22:07:00 +0900

    chore: error 처리

--- src/FirebaseInit.ts
+++ src/FirebaseInit.ts
@@ -1,12 +1,16 @@
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 // Import the functions you need from the SDKs you need
 export const getFireBaseToken = async () => {
-  const messaging = getMessaging();
-  const token = await getToken(messaging, {
-    vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY,
-  });
-  onMessage(messaging, (payload) => {
-    console.log('Message recieved. ', payload);
-  });
-  return token;
+  try {
+    const messaging = getMessaging();
+    const token = await getToken(messaging, {
+      vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY,
+    });
+    onMessage(messaging, (payload) => {
+      console.log('Message recieved. ', payload);
+    });
+    return token;
+  } catch (err: any) {
+    throw new Error('오류', err);
+  }
 };
